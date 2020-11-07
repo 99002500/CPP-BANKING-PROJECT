bacbbd375272c466df1c8c839215a574310e4ca4
99002500 <sampreeth.rayadurga@ltts.com>
2020-11-07 10:23:38 +0530

    defined the body of functions

--- banking.cc
+++ banking.cc
@@ -140,7 +140,35 @@ int main()
            return 0;
            }
 
-           
+           // Defining body of functions
+
+    Account::Account(string fname, string lname, float balance)
+    {
+     NextAccountNumber++;
+     accountNumber=NextAccountNumber;
+     firstName=fname;
+     lastName=lname;
+     this->balance=balance;
+    }
+    void Account::Deposit(float amount)
+    {
+        balance+=amount;
+    }
+    void Account::Withdraw(float amount)
+    {
+        if(balance-amount<MIN_BALANCE)
+        throw InsufficientFunds();
+        balance-=amount;
+    }
+    void Account::setLastAccountNumber(long accountNumber)
+    {
+        NextAccountNumber = accountNumber;
+    }
+    long Account::getLastAccountNumber()
+    {
+        return NextAccountNumber;
+    }
+    
 
 
 
